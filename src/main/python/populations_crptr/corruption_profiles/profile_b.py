6e15a79f0ba75fe79f2a1cf453b8d77933eb49f0
jar35 <jar35@st-andrews.ac.uk>
2025-06-24 13:32:25 +0100

    Implemented example corruption profiles
    Implemented configuration classes to represent the example corruption
    profiles, as defined in Tom's PHD thesis

---
+++ src/main/python/populations_crptr/corruption_profiles/profile_b.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# Author: James Ross (jar35@st-andrews.ac.uk)
+# 24/6/2015
+#
+# A config object implemention of the corruption profile 'B', as defined in Tom
+# Daltons thesis (see 7.4)
+# https://research-repository.st-andrews.ac.uk/bitstream/handle/10023/26784/Thesis-Tom-Dalton-complete-version.pdf?sequence=4&isAllowed=y
+
+class CorruptionProfileB {
+    # 0 for Standard human transcription, 1 for OCR
+    TYPE = 0
+    PROPORTION_TO_CORRUPT = 0.4
+    MAX_MODIFICATIONS_PER_ATTR = 2
+    MODIFICATIONS_PER_RECORD = 4
+    ATTR_LEVEL_PROPORTION = 0.75
+    RECORD_LEVEL_PROPORTION = 0.25
+}
